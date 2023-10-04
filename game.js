4dbf0d9811986a21277214a1118ecf3a6b110925
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-10-04 20:40:16 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,43 +1,36 @@
+// Constants for gameplay
+const MOVE_SPEED = 120;
+const JUMP_FORCE = 360;
+const BIG_JUMP_FORCE = 550;
+let CURRENT_JUMP_FORCE = JUMP_FORCE;
+const FALL_DEATH = 400;
+const ENEMY_SPEED = 20;
+
+// State variables
+let isJumping = true;
+
+// Initialize Kaboom
 kaboom({
-    global: true,
-    fullscreen: true,
-    scale: 2,
-    debug: true,
-    clearColor: [0, 0, 0, 1],
-  })
-  
-  
-  const MOVE_SPEED = 120
-  const JUMP_FORCE = 360
-  const BIG_JUMP_FORCE = 550
-  let CURRENT_JUMP_FORCE = JUMP_FORCE
-  const FALL_DEATH = 400
-  const ENEMY_SPEED = 20
-  
-  let isJumping = true
+  global: true,
+  fullscreen: true,
+  scale: 2,
+  debug: true,
+  clearColor: [0, 0, 0, 1],
+});
 
-loadRoot('https://i.imgur.com/')
-loadSprite('coin', 'wbKxhcd.png')
-loadSprite('evil-shroom', 'KPO3fR9.png')
-loadSprite('brick', 'pogC9x5.png')
-loadSprite('block', 'M6rwarW.png')
-loadSprite('mario', 'Wb1qfhK.png')
-loadSprite('mushroom', '0wMd92p.png')
-loadSprite('surprise', 'gesQ1KP.png')
-loadSprite('unboxed', 'bdrLpi6.png')
-loadSprite('pipe-top-left', 'ReTPiWY.png')
-loadSprite('pipe-top-right', 'hj2GK4n.png')
-loadSprite('pipe-bottom-left', 'c1cYSbt.png')
-loadSprite('pipe-bottom-right', 'nqQ79eI.png')
-// blue for next level 
-loadSprite('blue-block', 'fVscIbn.png')
-loadSprite('blue-brick', '3e5YRQd.png')
-loadSprite('blue-steel', 'gqVoI2b.png')
-loadSprite('blue-evil-shroom', 'SvV4ueD.png')
-loadSprite('blue-surprise', 'RMqCc1G.png')
+// Load sprites
+loadRoot('https://i.imgur.com/');
+[
+  'coin', 'evil-shroom', 'brick', 'block', 'mario', 'mushroom', 'surprise',
+  'unboxed', 'pipe-top-left', 'pipe-top-right', 'pipe-bottom-left', 'pipe-bottom-right',
+  'blue-block', 'blue-brick', 'blue-steel', 'blue-evil-shroom', 'blue-surprise'
+].forEach((spriteName) => {
+  loadSprite(spriteName, `${spriteName}.png`);
+});
 
+// Define the game scene
 scene("game", ({ level, score }) => {
-  layers(['bg', 'obj', 'ui'], 'obj')
+  layers(['bg', 'obj', 'ui'], 'obj');
 
   const maps = [
     [
@@ -64,7 +57,7 @@ scene("game", ({ level, score }) => {
       '£               z   z  x x x x x  x   ()£',
       '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
     ]
-  ]
+  ];
 
   const levelCfg = {
     width: 20,
@@ -85,4 +78,8 @@ scene("game", ({ level, score }) => {
     'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
     '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
     'x': [sprite('blue-steel'), solid(), scale(0.5)],
-  }
+  };
+});
+
+// Start the game scene
+go("game", { level: 0, score: 0 });
